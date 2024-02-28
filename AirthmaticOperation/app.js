086bceb037b06882411dab373f9e8253de9e8240
TouseefAbid <touseefabid737@gmail.com>
2024-02-28 15:17:27 +0500

    console code run

--- AirthmaticOperation/app.js
+++ AirthmaticOperation/app.js
@@ -1,49 +1,49 @@
 // Question 1
 
-// var num1 = 25
-// var num2 = 7
-// console.log(num1 % num2)
+var num1 = 25
+var num2 = 7
+console.log(num1 % num2)
 
 // Ans:4
 
 
 // Question 2
 
-// var num1 = 15
-// var num2 = 4
+var num1 = 15
+var num2 = 4
 
-// var number1 = num1 + 10
-// var number2 = num2 - 2
-// console.log( number1 * number2 )
+var number1 = num1 + 10
+var number2 = num2 - 2
+console.log( number1 * number2 )
 
 // Ans:50
 
 
 // Question 3
 
-// var num1 = 12
-// var num2 = 3
-// console.log(num1 ** num2)
+var num1 = 12
+var num2 = 3
+console.log(num1 ** num2)
 
 // Ans:1728
 
 
 // Question 4
 
-// var num1 = 10
-// var num2 = 3
-// console.log(num1 += num2 *2)
+var num1 = 10
+var num2 = 3
+console.log(num1 += num2 *2)
 
 // Ans:16
 
 
 // Question 5
 
-// var num1 = 20
-// var num2 = 4
+var num1 = 20
+var num2 = 4
 
-// var adding = num1 + 5
-// console.log(adding / num2)
+var adding = num1 + 5
+console.log(adding / num2)
 
 // Ans:6.25
 
@@ -51,18 +51,18 @@
 
 // Question 7
 
-// var num1 = 18
-// var num2 = 5
-// console.log( num1 - num2 + 2 )
+var num1 = 18
+var num2 = 5
+console.log( num1 - num2 + 2 )
 
 // Ans:15
 
 
 // Question 8
 
-// var num1 = 7
-// var num2 = 2
-// console.log(num1 ** num2)
+var num1 = 7
+var num2 = 2
+console.log(num1 ** num2)
 
 // Ans:49
 
@@ -70,11 +70,11 @@
 
 // Question 9
 
-// var num1 = 13
-// var num2 = 4
+var num1 = 13
+var num2 = 4
 
-// var math = num1 % (num2 + 2)
-// console.log(math)
+var math = num1 % (num2 + 2)
+console.log(math)
 
 // Ans:1
 
@@ -83,11 +83,11 @@
 // Question 11
 
 
-// var num1 = 25
-// var num2 = 3
+var num1 = 25
+var num2 = 3
 
-// var subtract = num1 - 1
-// console.log( subtract / num2 )
+var subtract = num1 - 1
+console.log( subtract / num2 )
 
 // Ans:8
 
@@ -95,33 +95,33 @@
 
 // Question 12
 
-// var num1 = 11
-// var num2 = 3
+var num1 = 11
+var num2 = 3
 
-// var adding = 2 + num2
-// console.log( num1 - adding )
+var adding = 2 + num2
+console.log( num1 - adding )
 
 // Ans:6
 
 
 // Question 13
 
-// var w = 22
-// var x = 5
+var w = 22
+var x = 5
 
-// var power = (x ** 2)
-// console.log( w + power )
+var power = (x ** 2)
+console.log( w + power )
 
 // Ans:47
 
 
 // Question 15
 
-// var a = 10
-// var b = 3
+var a = 10
+var b = 3
 
-// var subtract = (b - 2)
-// alert (a * subtract)
+var subtract = (b - 2)
+console.log (a * subtract)
 
 // Ans:10
 
