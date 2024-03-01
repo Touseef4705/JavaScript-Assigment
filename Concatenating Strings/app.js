e5adfc46057885eeba5421d77bb5c4645a3dd3c6
TouseefAbid <touseefabid737@gmail.com>
2024-03-01 15:17:41 +0500

    ConcatenatingStrings Added

---
+++ Concatenating Strings/app.js
@@ -0,0 +1,15 @@
+var tableNum = prompt('Which Table Do You Want?')
+var multiNum = 1
+var tnumber = 1
+
+document.write( ` <h3> ${tableNum} Ka Table </h3>  ` ) 
+document.write( ` <li> ${tableNum}  x ${tnumber} = ${tableNum * multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
+document.write( ` <li> ${tableNum} x ${++tnumber} = ${tableNum * ++multiNum} </li> ` )
