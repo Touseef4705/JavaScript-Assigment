3f89de5838892a61bbd60690b25716f7bd665388
TouseefAbid <touseefabid737@gmail.com>
2024-03-08 00:21:09 +0500

    prompt & else added

---
+++ Promot & If Else/app.js
@@ -0,0 +1,85 @@
+// Question:01
+
+// var num1 = prompt("Give number and check if is even or Odd")
+
+// if( num1 % 2 == 0 ){
+//     document.write(` <h3> ${num1} The Number Is Even </h3> `)
+// } else {
+//     document.write(` <h3> ${num1} The Number Is Odd </h3> `)
+// }
+
+// Question:02
+
+// var firstNum = prompt("Check if a Number is Positive, Negative, or Zero")
+
+// if ( firstNum > 0 ) {
+//     document.write(` <h3> ${firstNum} Is a Positive Number </h3> `)
+// } else if ( firstNum == 0 ) {
+//     document.write(` <h3> ${firstNum} Is a Zero Number </h3> `)
+// } else if ( firstNum < 0 ) {
+//     document.write(` <h3> ${firstNum} Is a Negative Number </h3> `)
+// }
+
+
+// Question:09
+
+// var mark1 = +prompt( "Enter a Math Subject Number" )
+// var mark2 = +prompt( "Enter a Urdu Subject Number" )
+// var mark3 = +prompt( "Enter a English Subject Number" )
+
+// var obtainMark = mark1 + mark2 + mark3
+// var totalMark = 300
+
+// var percentage = obtainMark /  totalMark * 100
+
+// if (percentage > 100){
+//     document.write(`Try Again!!`)
+// } else if ( percentage >= 80 ){
+//     document.write(` Congrats! Your Grade Is "A+" `)
+// } else if (percentage >= 70) {
+//     document.write(` Congrats! Your Grade Is "B" `)
+// } else if (percentage >= 60) {
+//     document.write(` Congrats! Your Grade Is "C" `)
+// } else if (percentage >= 50) {
+//     document.write(` Congrats! Your Grade Is "D" `)
+// } else {
+//     document.write(`Sorry You Are "Fail!"`)
+// }
+
+
+// Question:15
+
+// var userAge = +prompt("Enter Your Age")
+
+// if (userAge < 0) {
+//     document.write(`Sorry You Are Not Live In This Earth`)
+// } else if ( userAge <= 12) {
+//     document.write(` You Are Child `)
+// } else if ( userAge <= 19) {
+//     document.write(` You Are Tennager `)
+// } else if ( userAge <= 64) {
+//     document.write(` You Are Adult `)
+// } else if ( userAge > 64 ) {
+//     document.write(` You Are Almost Died `)
+// }
+
+// Question:14
+
+// var num = +prompt("Enter The Base Number")
+// var num1 = +prompt("Enter The Power Number")
+// var power = num ** num1
+// document.write(` <h1> ${power} </h1> `)
+
+
+
+// Question:13
+
+// var chracter = prompt("Find Vowels And Consonant Character")
+
+// if ( chracter == "a" || chracter == "e" || chracter == "i" || chracter == "o" || chracter == "u" 
+//     || chracter == "A" || chracter == "E" || chracter == "I" || chracter == "O" || chracter == "U" ) {
+//     document.write(` <h1> "${chracter}" Is a Vowels </h1> `)
+// } else {
+//     document.write(` <h1> "${chracter}" Is a Consonant </h1> `)
+// }
+
